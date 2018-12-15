13de7cc8203e9962503cad91b27e96082b2d7c8b
Reomor <reomor@gmail.com>
2018-12-15 15:23:10 -0500

    HW13star ansible scripts

--- docker-monolith/infra/terraform/main.tf
+++ docker-monolith/infra/terraform/main.tf
@@ -9,7 +9,7 @@ resource "google_compute_instance" "docker" {
   name         = "docker-${count.index}"
   machine_type = "g1-small"
   zone         = "${var.zone}"
-  tags         = ["docker-host"]
+  tags         = ["docker"]
 
   metadata {
     ssh-keys = "appuser:${file(var.public_key_path)}"
@@ -28,4 +28,17 @@ resource "google_compute_instance" "docker" {
       # ephemeral
     }
   }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["docker"]
 }
